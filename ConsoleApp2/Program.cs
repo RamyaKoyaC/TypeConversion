76ced44603540a2eb0899903ed0d5c1b8b13bb52
Ramya <ramyakrc10@gmail.com>
2018-03-03 21:17:11 -0600

    Code added

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Inttoboolean
+    {
+        static void Main(string[] args)
+        {
+          int d = 0;
+            
+            bool b = Convert.ToBoolean(d);
+          Console.WriteLine(b);
+          Console.Read();
+        }
+    }
+}
